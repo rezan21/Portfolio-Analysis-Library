0c711d1add22ce759aeee82be20a16e8ec30ca87
rezan21 <32329310+rezan21@users.noreply.github.com>
2021-11-27 05:51:42 +0000

    unittest to pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
-import unittest
-
 import numpy as np
+import pytest
 from src import main, utils
 
 from .desired import Desired
@@ -9,93 +8,104 @@ ffme = utils.load_ffme_returns()
 hfi = utils.load_hfi_returns()
 
 
-class TestPortfolioClass(unittest.TestCase):
-    def test_analyse_returns_annual_ret(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_ANN_RET
-        actual = ffme_portfolio.analysed_returns["annual_ret"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_analyse_returns_annual_ret():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_ANN_RET
+    actual = ffme_portfolio.analysed_returns["annual_ret"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_analyse_returns_per_period_ret(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_PER_PERIOD_RET
-        actual = ffme_portfolio.analysed_returns["per_period_ret"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_analyse_risk_annual_vol(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_ANN_VOL
-        actual = ffme_portfolio.analysed_risk["annual_vol"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_analyse_returns_per_period_ret():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_PER_PERIOD_RET
+    actual = ffme_portfolio.analysed_returns["per_period_ret"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_analyse_risk_semi_dev(self):
-        hfi_portfolio = main.Portfolio(hfi, 12)
-        desired = Desired.HFI_SEMI_DEV
-        actual = hfi_portfolio.analysed_risk["semi_dev"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_calculate_annual_sharpe_explicit(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_ANN_SHARPE
-        ffme_portfolio.analysed_returns
-        ffme_portfolio.analysed_risk
-        ffme_portfolio.get_annual_sharpe()
-        actual = ffme_portfolio.annual_sharpe.to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_analyse_risk_annual_vol():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_ANN_VOL
+    actual = ffme_portfolio.analysed_risk["annual_vol"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_calculate_annual_sharpe_implicit(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_ANN_SHARPE
-        ffme_portfolio.get_annual_sharpe()
-        actual = ffme_portfolio.annual_sharpe.to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_calculate_annual_sharpe_3pct_implicit(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_ANN_SHARPE_3PCT
-        ffme_portfolio.get_annual_sharpe(0.03)
-        actual = ffme_portfolio.annual_sharpe.to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_analyse_risk_semi_dev():
+    hfi_portfolio = main.Portfolio(hfi, 12)
+    desired = Desired.HFI_SEMI_DEV
+    actual = hfi_portfolio.analysed_risk["semi_dev"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_simulate_wealth_index(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_WEALTH_INDEX
-        ffme_portfolio.get_wealth_index(1000)
-        actual = ffme_portfolio.wealth_index["largeCap"].tolist()[:5]
-        np.testing.assert_almost_equal(desired, actual)
 
-    def test_calculate_max_drawdown_explicit(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_MAX_DRAWDOWN
-        ffme_portfolio.get_prev_high(ffme_portfolio.get_wealth_index(1000))
-        ffme_portfolio.get_max_drawdown(A=1000)
-        actual = ffme_portfolio.drawdown["max_drawdown"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_calculate_annual_sharpe_explicit():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_ANN_SHARPE
+    ffme_portfolio.analysed_returns
+    ffme_portfolio.analysed_risk
+    ffme_portfolio.get_annual_sharpe()
+    actual = ffme_portfolio.annual_sharpe.to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_calculate_max_drawdown_implicit(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_MAX_DRAWDOWN
-        ffme_portfolio.get_max_drawdown(A=1000)
-        actual = ffme_portfolio.drawdown["max_drawdown"].to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_calculate_max_drawdown_index(self):
-        ffme_portfolio = main.Portfolio(ffme, 12)
-        desired = Desired.FFME_MAX_DRAWDOWN_PERIOD  # aka index
-        ffme_portfolio.get_max_drawdown(A=1000)
-        actual = ffme_portfolio.drawdown["id_max_drawdown"].to_dict()
-        np.testing.assert_equal(actual, desired)
+def test_calculate_annual_sharpe_implicit():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_ANN_SHARPE
+    ffme_portfolio.get_annual_sharpe()
+    actual = ffme_portfolio.annual_sharpe.to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_get_skewness(self):
-        hfi_portfolio = main.Portfolio(hfi, 12)
-        desired = Desired.HFI_SKEWNESS
-        hfi_portfolio.get_skewness()
-        actual = hfi_portfolio.skewness.sort_values().to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
 
-    def test_get_kurtosis(self):
-        hfi_portfolio = main.Portfolio(hfi, 12)
-        desired = Desired.HFI_KURTOSIS
-        hfi_portfolio.get_kurtosis(excess=False)
-        actual = hfi_portfolio.kurtosis.to_dict()
-        utils.numpy_assert_almost_dicts(desired, actual)
+def test_calculate_annual_sharpe_3pct_implicit():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_ANN_SHARPE_3PCT
+    ffme_portfolio.get_annual_sharpe(0.03)
+    actual = ffme_portfolio.annual_sharpe.to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
+
+
+def test_simulate_wealth_index():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_WEALTH_INDEX
+    ffme_portfolio.get_wealth_index(1000)
+    actual = ffme_portfolio.wealth_index["largeCap"].tolist()[:5]
+    np.testing.assert_almost_equal(desired, actual)
+
+
+def test_calculate_max_drawdown_explicit():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_MAX_DRAWDOWN
+    ffme_portfolio.get_prev_high(ffme_portfolio.get_wealth_index(1000))
+    ffme_portfolio.get_max_drawdown(A=1000)
+    actual = ffme_portfolio.drawdown["max_drawdown"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
+
+
+def test_calculate_max_drawdown_implicit():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_MAX_DRAWDOWN
+    ffme_portfolio.get_max_drawdown(A=1000)
+    actual = ffme_portfolio.drawdown["max_drawdown"].to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
+
+
+def test_calculate_max_drawdown_index():
+    ffme_portfolio = main.Portfolio(ffme, 12)
+    desired = Desired.FFME_MAX_DRAWDOWN_PERIOD  # aka index
+    ffme_portfolio.get_max_drawdown(A=1000)
+    actual = ffme_portfolio.drawdown["id_max_drawdown"].to_dict()
+    np.testing.assert_equal(actual, desired)
+
+
+def test_get_skewness():
+    hfi_portfolio = main.Portfolio(hfi, 12)
+    desired = Desired.HFI_SKEWNESS
+    hfi_portfolio.get_skewness()
+    actual = hfi_portfolio.skewness.sort_values().to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
+
+
+def test_get_kurtosis():
+    hfi_portfolio = main.Portfolio(hfi, 12)
+    desired = Desired.HFI_KURTOSIS
+    hfi_portfolio.get_kurtosis(excess=False)
+    actual = hfi_portfolio.kurtosis.to_dict()
+    utils.numpy_assert_almost_dicts(desired, actual)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,41 +1,39 @@
-import unittest
-
 import numpy as np
 import pandas as pd
+import pytest
 from src import utils
 
 
-class TestFFME(unittest.TestCase):
-    def test_load_ffme_returns(self):
-        df = utils.load_ffme_returns()
-        assert df.shape == (1110, 2)
-        assert df.index[0] == pd.Period("1926-07")
-        assert df.index[-1] == pd.Period("2018-12")
-        assert df.columns.tolist() == ["smallCap", "largeCap"]
+def test_load_ffme_returns():
+    df = utils.load_ffme_returns()
+    assert df.shape == (1110, 2)
+    assert df.index[0] == pd.Period("1926-07")
+    assert df.index[-1] == pd.Period("2018-12")
+    assert df.columns.tolist() == ["smallCap", "largeCap"]
 
 
-class TestHFI(unittest.TestCase):
-    def test_load_hfi_returns(self):
-        df = utils.load_hfi_returns()
-        assert df.shape == (263, 13)
-        assert df.index[0] == pd.Period("1997-01")
-        assert df.index[-1] == pd.Period("2018-11")
+def test_load_hfi_returns():
+    df = utils.load_hfi_returns()
+    assert df.shape == (263, 13)
+    assert df.index[0] == pd.Period("1997-01")
+    assert df.index[-1] == pd.Period("2018-11")
 
 
-class TestDictRelated(unittest.TestCase):
-    def test_dict_to_keys_values(self):
-        d = {"a": 1, "b": 2, "c": 3, "d": None}
-        keys, values = utils.dict_to_keys_values(d)
-        assert keys == ["a", "b", "c", "d"]
-        assert values == [1, 2, 3, None]
+def test_dict_to_keys_values():
+    d = {"a": 1, "b": 2, "c": 3, "d": None}
+    keys, values = utils.dict_to_keys_values(d)
+    assert keys == ["a", "b", "c", "d"]
+    assert values == [1, 2, 3, None]
 
-    def test_numpy_assert_almost_dicts(self):
-        dict1 = {"b": np.array([1, 2, 0.2])}
-        dict2 = {"b": np.array([1, 2, 3 * 0.1 - 0.1])}  # almost same
-        dict3 = {"b": np.array([999, 888, 444])}  # completely different
 
-        # no exception expected
-        utils.numpy_assert_almost_dicts(dict1, dict2)
+def test_numpy_assert_almost_dicts():
+    dict1 = {"b": np.array([1, 2, 0.2])}
+    dict2 = {"b": np.array([1, 2, 3 * 0.1 - 0.1])}  # almost same
+    dict3 = {"b": np.array([999, 888, 444])}  # completely different
 
-        # assert for expected failure
-        self.assertRaises(AssertionError, utils.numpy_assert_almost_dicts, dict1, dict3)
+    # no exception expected
+    utils.numpy_assert_almost_dicts(dict1, dict2)
+
+    # assert for expected failure
+    with pytest.raises(AssertionError):
+        utils.numpy_assert_almost_dicts(dict1, dict3)
