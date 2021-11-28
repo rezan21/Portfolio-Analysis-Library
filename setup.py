2e4f0c242aa25ef4c24e02dd00178b8e35451228
rezan21 <32329310+rezan21@users.noreply.github.com>
2021-11-28 23:52:44 +0000

    manifest

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as file:
-    long_description = file.read()
+    LONG_DESCRIPTION = file.read()
 
-# # to-do: separate dev requirements
+with open("requirements.txt", "r") as file:
+    ALL_REQS = file.read().split("\n")
+    ALL_REQS = [req for req in ALL_REQS if req]
+    # to-do: separate dev requirements
+    INSTALL_REQUIRES = ALL_REQS
+    EXTRAS_REQUIRE = ALL_REQS
+
+
+def get_src_modules():
+    py_files = Path("./src/").glob("*.py")
+    py_files = list(map(lambda file: file.stem, py_files))  # get file names
+    py_files.remove("__init__")  # remove __init__.py
+    return py_files
+
+
 setup(
     name="portfolio-analysis-library",
     description="#WIP: Analysing Stock Returns & Constructing Portfolios with Python",
@@ -11,16 +27,17 @@ setup(
     author="Reza N",
     author_email="32329310+rezan21@users.noreply.github.com",
     url="https://github.com/rezan21/Portfolio-Analysis-Library",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.6",
     # py_modules=["analysis", "log", "main", "pal", "params", "utils"],
     # package_dir={"": "src"},
-    install_requires=["numpy", "pandas", "matplotlib", "seaborn", "tqdm"],
-    extras_require={"dev": ["pytest"]},
+    install_requires=INSTALL_REQUIRES,
+    extras_require={"dev": EXTRAS_REQUIRE},
     classifiers=[
         "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
